3e61b730fa58da435a0f2b5b972357f1f14e1732
Mohamedtharwat8 <m.tharwat897@gmail.com>
2023-06-12 08:38:46 +0300

    Add project files.

---
+++ API/Models/Context.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Models
+{
+    public class Context : IdentityDbContext
+    {
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LasttName { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    }
+}
